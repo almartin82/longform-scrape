5e04239d981a4d00ded9ff4c2afb408000a812a9
almartin82 <almartin@gmail.com>
2015-04-19 00:09:34 -0400

    add pub_date

--- longform_scrape.py
+++ longform_scrape.py
@@ -11,12 +11,14 @@ def parse_article(requests_content, i):
     article = soup.find("div", attrs={'class': 'content'})
     title = article.find("h2").text
     author = article.find("span", attrs={'class': 'byline'}).text
-    pub = article.find("span", attrs={'class': 'publication'}).text
+    pub_date = article.find("span", attrs={'class': 'publication_date'}).text
+    pub_name = article.find("span", attrs={'class': 'publication'}).text
     summary = article.find("div", attrs={'class': 'body'}).text
     parsed = pandas.DataFrame({
         'title': title,
         'author': author,
-        'publication': pub,
+        'pub_date': pub_date,
+        'publication': pub_name,
         'summary': summary
     }, index = [i])
     return parsed
@@ -25,7 +27,7 @@ def parse_article(requests_content, i):
 df = pandas.DataFrame()
 
 start = 1
-limit = 8224
+limit = 8227
 #testing only
 # start = 8000
 # limit = 8010
@@ -45,4 +47,4 @@ for i in range(start, limit):
     #don't hammer them
     time.sleep(0.5)
 
-df.to_csv('datasets/longform_sources.csv', sep=',', quoating=csv.QUOTE_ALL, encoding='utf-8')
+df.to_csv('longform_sources.csv', sep=',', quoating=csv.QUOTE_ALL, encoding='utf-8')
